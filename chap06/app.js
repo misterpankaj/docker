49a7b8df2e8cd43dc3a814a08a2dd70c1497214d
Pankaj Gupta <misterpankaj2002@gmail.com>
2023-04-02 13:37:27 -0400

    updated code

--- chap06/app.js
+++ chap06/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const mongoose = require('mongoose');
+const fs = require('fs')
 
 const app = express();
 
@@ -13,6 +14,41 @@ app.get('/fetchdata', async (req, res) => {
     res.status(500).send('Error retrieving data');
   }
 });
+
+app.get('/api/data', (req, res) => {
+  const keyvalue = req.query.param1;
+  const file = req.query.param2;
+
+  console.log('Query params1 keyvalue:', keyvalue);
+  console.log('Query params2 filename:', file);
+
+  app.use('/test', express.static('test'));
+  app.use('/perm', express.static('perm'));
+  
+
+  const folder1 = 'test';
+  const folder2 = 'perm';
+  const fileName = file+".txt"
+
+
+  if (keyvalue === 'test') {
+    fs.writeFile(`${folder1}/${fileName}`, 'test'+file, (err) => {
+      if (err) throw err;
+      console.log(`${folder1}/${fileName} has been created in ${folder1}`);
+    });
+  } else if (keyvalue === 'perm') {
+    fs.writeFile(`${folder2}/${fileName}`, 'perm'+file, (err) => {
+      if (err) throw err;
+      console.log(`${folder2}/${fileName} has been created in 1111${folder2}`);
+    });
+  } else {
+    console.log(`Invalid variable name: ${variableName}`);
+  }
+
+  res.send('Response data 100');
+});
+
+
 
 // Connect to local MongoDB database
 // host.docker.internal
